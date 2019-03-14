ec84059f0ef40022f2188f2700a22dac5ecd6046
Luciano Lima <beavess@gmail.com>
2019-03-14 15:37:37 -0300

    Add GoogleSignin for React Native

--- ios/Example/AppDelegate.m
+++ ios/Example/AppDelegate.m
@@ -10,6 +10,7 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <RNGoogleSignin/RNGoogleSignin.h>
 
 @implementation AppDelegate
 
@@ -32,6 +33,13 @@
   [self.window makeKeyAndVisible];
   [FIRApp configure];
   return YES;
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(nonnull NSURL *)url options:(nonnull NSDictionary<NSString *,id> *)options {
+  return [RNGoogleSignin application:application
+                             openURL:url
+                   sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
+                          annotation:options[UIApplicationOpenURLOptionsAnnotationKey]];
 }
 
 @end
